11f3538df45d2a680efbc77e8b114b1dafab4b17
Nitya Krushna <iamnityakrushna@gmail.com>
2021-04-05 11:21:12 +0530

    this is from second workspace- added manual file

---
+++ src/test/java/qaEndToEndDummyFramework/ManualTesting.java
@@ -0,0 +1,16 @@
+package qaEndToEndDummyFramework;
+
+public class ManualTesting {
+
+	
+	public void  functionalTesting() {
+		
+		System.out.println("This is functional testing");
+	}
+	
+    public void  NonfunctionalTesting() {
+		
+		System.out.println("This is non functional testing");
+	}
+	
+}
